428be00bdd9ab5c122306efa32cc9ccd51c03e02
k8w <me@k8w.io>
2021-04-23 21:16:06 +0800

    rm OLD

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import 'k8w-extend-native';
-export { HttpClient, HttpClient as TsrpcClient } from "../OLD/HttpClient";
+export { HttpClient, HttpClient as TsrpcClient } from "./HttpClient";
 export { WsClient } from './WsClient';
 
