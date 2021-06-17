968ccea3d7d21eddeae85d18be671a5785746f1a
k8w <me@k8w.io>
2021-06-17 23:24:56 +0800

    3.0.3 pre-babel-polyfill

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
+import { babel } from '@rollup/plugin-babel';
 import typescript from 'rollup-plugin-typescript2';
-
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 export default [
     {
         input: './src/index.ts',
@@ -17,9 +19,12 @@ export default [
                         module: "esnext"
                     }
                 }
-            })
+            }),
+            nodeResolve(),
+            commonjs(),
+            babel({ babelHelpers: 'bundled' })
         ],
-        external: ['tslib']
+        external: ['tslib', 'k8w-extend-native', /core\-js/]
     },
     {
         input: './src/index.ts',
@@ -37,8 +42,11 @@ export default [
                         module: "esnext"
                     }
                 }
-            })
+            }),
+            nodeResolve(),
+            commonjs(),
+            babel({ babelHelpers: 'bundled' })
         ],
-        external: ['tslib']
+        external: ['tslib', 'k8w-extend-native', /core\-js/]
     }
 ]
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ import 'k8w-extend-native';
 export * from 'tsrpc-proto';
 export * from "./client/HttpClient";
 export * from './client/WsClient';
+
