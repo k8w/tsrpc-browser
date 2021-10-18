9f5f6d87abc06e1bf6e057d0539b4b1414de0d42
k8w <me@k8w.io>
2021-10-18 22:15:28 +0800

    3.0.11 publish

--- scripts/postDts.js
+++ scripts/postDts.js
@@ -6,10 +6,11 @@ let content = fs.readFileSync(path.resolve(__dirname, '../dist/index.d.ts'), 'ut
 
 // removePrivate
 content = content.replace(/\s+(private).+;/g, '');
-content = require('./copyright') + '\n' + content + `\n/// <reference path="./mongodb.d.ts" />\n`;
+content = require('./copyright') + '\n' + content;
 
 // Copy mongodb.d.ts
-fs.copyFileSync(path.resolve(__dirname, '../src/mongodb.d.ts'), path.resolve(__dirname, '../dist/mongodb.d.ts'));
+// let mongodb = fs.readFileSync(path.resolve(__dirname, '../src/mongodb.d.ts'), 'utf-8');
+// content = content + '\n' + mongodb + '\n';
 
 // Update index.d.ts
 fs.writeFileSync(path.resolve(__dirname, '../dist/index.d.ts'), content, 'utf-8');
--- src/mongodb.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'mongodb' {
-    // 在浏览器，ObjectID 被自动反序列化为字符串
-    export type ObjectId = string;
-    export type ObjectID = string;
-}
